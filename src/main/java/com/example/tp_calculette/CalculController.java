5ee59b9b6190a825f433c53fd376377a9f996f07
Dania Midera <daniamidera@gmail.com>
2025-11-10 22:02:39 +0100

    Interface calculette

--- src/main/java/com/example/tp_calculette/CalculController.java
+++ src/main/java/com/example/tp_calculette/CalculController.java
@@ -1,14 +1,17 @@
 package com.example.tp_calculette;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class CalculController {
-    @FXML
-    private Label welcomeText;
+    public TextField value1, value2, value3;
+    public Label response;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onClicked(){
+        response.setText("I am clicked here !");
     }
+
 }
