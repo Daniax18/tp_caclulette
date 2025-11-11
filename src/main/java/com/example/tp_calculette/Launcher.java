39f472997c6caef28d763cbe61c85ff4202873e7
TsioryRobson <tsioryrobson@gmail.com>
2025-11-11 15:43:33 +0300

    Feature/calcul: Architecture MVC

--- src/main/java/com/example/tp_calculette/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.tp_calculette;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(CalculApplication.class, args);
-    }
-}
